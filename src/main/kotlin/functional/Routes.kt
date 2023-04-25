01239549f9a985c88c7f92f335d6dd4145fee74e
James Richardson <james@time4tea.net>
2023-04-25 22:35:48 +0100

    copying routes where seem similar

--- src/main/kotlin/functional/Routes.kt
+++ src/main/kotlin/functional/Routes.kt
@@ -1,7 +1,20 @@
 package functional
 
+import org.http4k.core.HttpHandler
+import org.http4k.core.Method
+import org.http4k.core.Request
+import org.http4k.core.Response
+import org.http4k.core.Status
+import org.http4k.routing.bind
+import org.http4k.routing.routes
+
 
 object UserRoutes {
-
-
+    operator fun invoke():HttpHandler {
+        return routes(
+            "/" bind Method.GET to { _: Request -> Response(Status.OK) },
+            "/sse" bind Method.GET to { _: Request -> Response(Status.OK) },
+            "/users" bind Method.GET to { _: Request -> Response(Status.OK) },
+        )
+    }
 }
